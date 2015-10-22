d5e689e0cdebb5f66ed001daa965d07e12942784
David Keijser <david.keijser@klarna.com>
2015-10-22 21:17:16 +0200

    use the default mesos command executor

--- handler/task.go
+++ handler/task.go
@@ -2,57 +2,56 @@ package handler
 
 import (
 	"fmt"
+	log "github.com/dmuth/google-go-log4go"
 
 	"github.com/alde/eremetic/types"
 	"github.com/gogo/protobuf/proto"
+	"github.com/m4rw3r/uuid"
 	mesos "github.com/mesos/mesos-go/mesosproto"
 	"github.com/mesos/mesos-go/mesosutil"
 )
 
 type eremeticTask struct {
-	taskCPUs    float64
-	taskMem     float64
-	dockerImage string
-	command     string
-	executor    *mesos.ExecutorInfo
+	taskCPUs  float64
+	taskMem   float64
+	command   *mesos.CommandInfo
+	container *mesos.ContainerInfo
 }
 
 func createEremeticTask(request types.Request) eremeticTask {
 	task := eremeticTask{
-		taskCPUs:    request.TaskCPUs,
-		taskMem:     request.TaskMem,
-		dockerImage: request.DockerImage,
-		command:     request.Command,
-		executor: &mesos.ExecutorInfo{
-			ExecutorId: &mesos.ExecutorID{Value: proto.String("eremetic-executor")},
-			Command: &mesos.CommandInfo{
-				Value: proto.String(request.Command),
-				User:  proto.String("root"),
+		taskCPUs: request.TaskCPUs,
+		taskMem:  request.TaskMem,
+		command: &mesos.CommandInfo{
+			Value: proto.String(request.Command),
+			User:  proto.String("root"),
+		},
+		container: &mesos.ContainerInfo{
+			Type: mesos.ContainerInfo_DOCKER.Enum(),
+			Docker: &mesos.ContainerInfo_DockerInfo{
+				Image: proto.String(request.DockerImage),
 			},
-			Container: &mesos.ContainerInfo{
-				Type: mesos.ContainerInfo_DOCKER.Enum(),
-				Docker: &mesos.ContainerInfo_DockerInfo{
-					Image: proto.String(request.DockerImage),
-				},
-			},
-			Name: proto.String("Eremetic"),
 		},
 	}
 	return task
 }
 
 func createTaskInfo(task *eremeticTask, taskID int, offer *mesos.Offer) *mesos.TaskInfo {
-	id := proto.String(fmt.Sprintf(
-		"Eremetic-%d: Running '%s' on '%s'",
-		taskID, task.command, task.dockerImage))
+	randId, err := uuid.V4()
+	if err != nil {
+		log.Error("Could not create random Id")
+		os.Exit(1)
+	}
+	id := fmt.Sprintf("eremetic-task.%s", randId.String())
 
 	return &mesos.TaskInfo{
 		TaskId: &mesos.TaskID{
-			Value: id,
+			Value: proto.String(id),
 		},
-		SlaveId:  offer.SlaveId,
-		Name:     proto.String("EREMETIC_" + *id),
-		Executor: task.executor,
+		SlaveId:   offer.SlaveId,
+		Name:      proto.String("Eremetic task " + string(taskID)),
+		Command:   task.command,
+		Container: task.container,
 		Resources: []*mesos.Resource{
 			mesosutil.NewScalarResource("cpus", task.taskCPUs),
 			mesosutil.NewScalarResource("mem", task.taskMem),
