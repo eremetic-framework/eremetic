944dc13b68a7ae8ff8bdd687f8defc6116573d8d
David Keijser <david.keijser@klarna.com>
2015-10-23 13:12:03 +0200

    use default master detector over zook

--- handler/driver.go
+++ handler/driver.go
@@ -3,7 +3,6 @@ package handler
 import (
 	"net"
 
-	"github.com/alde/eremetic/zook"
 	"github.com/golang/protobuf/proto"
 	mesos "github.com/mesos/mesos-go/mesosproto"
 	sched "github.com/mesos/mesos-go/scheduler"
@@ -13,10 +12,9 @@ import (
 func createDriver(scheduler *eremeticScheduler) (*sched.MesosSchedulerDriver, error) {
 	publishedAddr := net.ParseIP(viper.GetString("messenger_address"))
 	bindingPort := uint16(viper.GetInt("messenger_port"))
-	master := zook.DiscoverMaster(viper.GetString("zookeeper"))
 
 	return sched.NewMesosSchedulerDriver(sched.DriverConfig{
-		Master: master,
+		Master: viper.GetString("zookeeper"),
 		Framework: &mesos.FrameworkInfo{
 			Name: proto.String("Eremetic"),
 			User: proto.String(""),
