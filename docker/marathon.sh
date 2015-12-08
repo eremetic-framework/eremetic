266c8b51b586751a6242528023f0bdeb9d570675
Mao Geng <gengmao@gmail.com>
2015-12-08 18:40:30 +0000

    Fix messenger address in container

--- docker/marathon.sh
+++ docker/marathon.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 lookup_host() {
-    nslookup "$1" | awk -v HOST="$1" '{ if ($2 == HOST) { getline; gsub(/^.*: /, ""); print; } }'
+    nslookup "$1" | awk -v HOST="$1" '{ if ($2 == HOST) { getline; gsub(/^.*: /, ""); split($0, a, " ", seps); print a[1]; } }'
 }
 
 export MESSENGER_ADDRESS=`lookup_host ${HOST}${DOMAIN:+.$DOMAIN}`
