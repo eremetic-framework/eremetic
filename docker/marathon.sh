e531ce6e5a44901c6c89a4d6e481d571123a8ad9
Mao Geng <gengmao@gmail.com>
2015-12-09 23:23:00 +0000

    Use a simpler awk expression

--- docker/marathon.sh
+++ docker/marathon.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 lookup_host() {
-    nslookup "$1" | awk -v HOST="$1" '{ if ($2 == HOST) { getline; gsub(/^.*: /, ""); split($0, a, " ", seps); print a[1]; } }'
+    nslookup "$1" | awk -v HOST="$1" '{ if ($2 == HOST) { getline; gsub(/^.*: /, ""); print $1 } }'
 }
 
 export MESSENGER_ADDRESS=`lookup_host ${HOST}${DOMAIN:+.$DOMAIN}`
