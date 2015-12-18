8cf992946c61c641aa06a5fa91733925dbabd1c5
David Keijser <david.keijser@klarna.com>
2015-12-18 11:10:13 +0100

    Collect basic metrics with prometheus

--- types/route.go
+++ types/route.go
@@ -4,10 +4,10 @@ import "net/http"
 
 // Route enforces the structure of a route
 type Route struct {
-	Name        string
-	Method      string
-	Pattern     string
-	HandlerFunc http.HandlerFunc
+	Name    string
+	Method  string
+	Pattern string
+	Handler http.Handler
 }
 
 // Routes is a collection of route structs
