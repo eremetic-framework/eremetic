1e9fd676fc167d589eba4e56d837c8125125bfd5
Rickard Dybeck <dybeck@spotify.com>
2020-04-13 11:44:21 -0400

    remove deprecated -e flag from docker login

--- misc/docker.sh
+++ misc/docker.sh
@@ -5,6 +5,6 @@ set -e
 test -n "$DOCKER_USERNAME" -a "$PUBLISH_VERSION" == "$TRAVIS_GO_VERSION"
 grep -E 'master|v[0-9.]+' > /dev/null <<< "$TRAVIS_BRANCH"
 
-docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 make docker
 make publish-docker
